0ae8017c41bb0c718e033817d1f33c20f53e03dc
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-03-07 23:32:26 +0300

    create frontend folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
+/*=================Components======================= */
+import NavBar from "./components/navbar/navbar";
+
 function App() {
   return (
     <div className="App">
       <h1>Start project 6</h1>
+      <NavBar/>
     </div>
   );
 }
---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+/** @format */
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+const NavBar = () => {
+  return <div className="NavBar">
+      <h1>nav</h1>
+      </div>;
+};
+
+export default NavBar;
