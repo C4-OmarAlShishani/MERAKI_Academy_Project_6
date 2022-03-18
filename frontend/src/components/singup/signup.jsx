f8962e1a9ce5bb4074a9089d241e3bd932cc9130
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-03-18 16:19:13 +0300

    register css

--- frontend/src/components/singup/signup.jsx
+++ frontend/src/components/singup/signup.jsx
@@ -68,159 +68,160 @@ const SignUp = () => {
   };
 
   return (
-    <div className="signUp">
-      <div className="group">
-        <input
-          onChange={(e) => {
-            setFirstName(e.target.value);
-          }}
-          value={firstName}
-          type="text"
-          placeholder="First Name"
-        />
+    // <div className="signUp">
+    //   <div className="group">
+    //     <input
+    //       onChange={(e) => {
+    //         setFirstName(e.target.value);
+    //       }}
+    //       value={firstName}
+    //       type="text"
+    //       placeholder="First Name"
+    //     />
 
-        <input
-          onChange={(e) => {
-            setLastName(e.target.value);
-          }}
-          value={lastName}
-          type="text"
-          placeholder="Last Name"
-        />
+    //     <input
+    //       onChange={(e) => {
+    //         setLastName(e.target.value);
+    //       }}
+    //       value={lastName}
+    //       type="text"
+    //       placeholder="Last Name"
+    //     />
 
-        <input
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          value={email}
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          value={password}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          onChange={(e) => {
-            setRepeatPassword(e.target.value);
-          }}
-          value={repeatPassword}
-          type="Password"
-          placeholder="Repeat password"
-        />
-        <input
-          type="file"
-          onChange={(e) => {
-            setImageURL(e.target.files[0]);
-          }}
-        />
-        <button
-          onClick={() => {
-            uploadImage();
-          }}>
-          Register
-        </button>
-      </div>
-      <section className="vh-100">
-        <div className="container-fluid h-custom">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-              <form onSubmit={createUser}>
-                <div className="form-outline mb-4">
-                  <input
-                    className="form-control form-control-lg"
-                    onChange={(e) => {
-                      setFirstName(e.target.value);
-                    }}
-                    value={firstName}
-                    type="text"
-                    placeholder="First Name"
-                  />
-                </div>
-                <div className="form-outline mb-3">
-                  <input
-                    className="form-control form-control-lg"
-                    onChange={(e) => {
-                      setLastName(e.target.value);
-                    }}
-                    value={lastName}
-                    type="text"
-                    placeholder="Last Name"
-                  />
-                </div>
-                <div className="form-outline mb-3">
-                  <input
-                    className="form-control form-control-lg"
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                    value={email}
-                    type="email"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="form-outline mb-3">
-                  <input
-                    className="form-control form-control-lg"
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                    value={password}
-                    type="password"
-                    placeholder="Password"
-                  />
-                </div>
-                <div className="form-outline mb-3">
-                  <input
-                    className="form-control form-control-lg"
-                    onChange={(e) => {
-                      setRepeatPassword(e.target.value);
-                    }}
-                    value={repeatPassword}
-                    type="Password"
-                    placeholder="Repeat password"
-                  />
-                </div>
-                <div className="text-center text-lg-start mt-4 pt-2">
-                  <button
-                    type="submit"
-                    className="btn btn-primary btn-lg"
-                    style={{ paddingLeft: "2.5rem", paddingRight: " 2.5rem" }}>
-                    Register
-                  </button>
-                  <div className="avatar-upload">
-                    <div className="avatar-edit">
-                      <input
-                        type="file"
-                        id="imageUpload"
-                        accept=".png, .jpg, .jpeg"
-                        onChange={(e) => {
-                          setImageURL(e.target.files[0]);
-                          uploadImage()
-                        }}
-                      />
-                      <label htmlFor="imageUpload"></label>
-                    </div>
-                    <div
-                      className="avatar-preview"
-                      style={{
-                        backgroundImage: `url(${image ? image : avatar})`,
-                        backgroundSize: "cover",
-                      }}>
-                      <div id="imagePreview"></div>
-                    </div>
-                  </div>
-                </div>
-              </form>
+    //     <input
+    //       onChange={(e) => {
+    //         setEmail(e.target.value);
+    //       }}
+    //       value={email}
+    //       type="email"
+    //       placeholder="Email"
+    //     />
+    //     <input
+    //       onChange={(e) => {
+    //         setPassword(e.target.value);
+    //       }}
+    //       value={password}
+    //       type="password"
+    //       placeholder="Password"
+    //     />
+    //     <input
+    //       onChange={(e) => {
+    //         setRepeatPassword(e.target.value);
+    //       }}
+    //       value={repeatPassword}
+    //       type="Password"
+    //       placeholder="Repeat password"
+    //     />
+    //     <input
+    //       type="file"
+    //       onChange={(e) => {
+    //         setImageURL(e.target.files[0]);
+    //       }}
+    //     />
+    //     <button
+    //       onClick={() => {
+    //         uploadImage();
+    //       }}>
+    //       Register
+    //     </button>
+    //   </div>
+    <section className="vh-600">
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            {/* <form onSubmit={createUser}> */}
+            <div className="form-outline mb-4">
+              <input
+                className="form-control form-control-lg"
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+                value={firstName}
+                type="text"
+                placeholder="First Name"
+              />
             </div>
+            <div className="form-outline mb-3">
+              <input
+                className="form-control form-control-lg"
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+                value={lastName}
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+            <div className="form-outline mb-3">
+              <input
+                className="form-control form-control-lg"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                value={email}
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-outline mb-3">
+              <input
+                className="form-control form-control-lg"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                value={password}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-outline mb-3">
+              <input
+                className="form-control form-control-lg"
+                onChange={(e) => {
+                  setRepeatPassword(e.target.value);
+                }}
+                value={repeatPassword}
+                type="Password"
+                placeholder="Repeat password"
+              />
+            </div>
+            <div className="text-center text-lg-start mt-4 pt-2">
+              <button
+                // type="submit"
+                onClick={() => createUser()}
+                className="btn btn-primary btn-lg"
+                style={{ paddingLeft: "2.5rem", paddingRight: " 2.5rem" }}>
+                Register
+              </button>
+            </div>
+            <div className="avatar-upload">
+              <div className="avatar-edit">
+                <input
+                  type="file"
+                  id="imageUpload"
+                  accept=".png, .jpg, .jpeg"
+                  onChange={(e) => {
+                    setImageURL(e.target.files[0]);
+                    uploadImage();
+                  }}
+                />
+                <label htmlFor="imageUpload"></label>
+              </div>
+              <div
+                className="avatar-preview"
+                style={{
+                  backgroundImage: `url(${image ? image : avatar})`,
+                  backgroundSize: "cover",
+                }}>
+                {/* <div id="imagePreview"></div> */}
+              </div>
+            </div>
+            {/* </form> */}
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+    // </div>
   );
 };
 
